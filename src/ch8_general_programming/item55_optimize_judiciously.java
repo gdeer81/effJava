edd7773a8c2743170f0dccd65cdf1bf11ad897bb
pance <pbarnett888@gmail.com>
2014-08-01 15:34:30 -0500

    Add to item55 with a quote from the book

---
+++ src/ch8_general_programming/item55_optimize_judiciously.java
@@ -0,0 +1,5 @@
+public class item55_optimize_judiciously {
+    public static void main(String[] args) {
+        System.out.println("do not strive to write fast programs—strive to write good ones; speed will follow.");
+    }
+}
