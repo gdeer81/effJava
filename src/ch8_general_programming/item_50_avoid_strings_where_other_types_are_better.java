84af1926681114fec3e52b12265b43924a03a3c9
Atlas- <c2.pound@gmail.com>
2014-08-05 15:28:37 -0500

    examples for item 50

--- src/ch8_general_programming/item_50_avoid_strings_where_other_types_are_better.java
+++ src/ch8_general_programming/item_50_avoid_strings_where_other_types_are_better.java
@@ -6,4 +6,22 @@
  * To change this template use File | Settings | File Templates.
  */
 public class item_50_avoid_strings_where_other_types_are_better {
+    public static void main(String[] args) {
+        System.out.println("Strings are poor substitutes for other value types");
+        // Inappropriate use of string as aggregate type
+//        String compoundKey = className + "#" + i.next();
+    }
+
+    // Broken - inappropriate use of string as capability!
+    public class ThreadLocal {
+        private ThreadLocal() {
+            System.out.println("what if 2 clients use the same key?");
+        }
+        // Sets the current thread's value for the named variable.
+//        public void set(String key, Object value);
+        // Returns the current thread's value for the named variable.
+//        public Object get(String key);
+        //whats the better way?
+
+    }
 }
