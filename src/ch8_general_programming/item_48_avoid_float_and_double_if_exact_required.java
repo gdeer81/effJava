8b0f90d81408a2ce60ec0d3c5c3ddf13a7cf93bd
Atlas- <c2.pound@gmail.com>
2014-08-05 13:46:35 -0500

    examples for item 48

--- src/ch8_general_programming/item_48_avoid_float_and_double_if_exact_required.java
+++ src/ch8_general_programming/item_48_avoid_float_and_double_if_exact_required.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 /**
  * Created by IntelliJ IDEA.
  * User: p590032
@@ -6,4 +8,24 @@
  * To change this template use File | Settings | File Templates.
  */
 public class item_48_avoid_float_and_double_if_exact_required {
+
+    public static void main(String[] args) {
+        System.out.println("float and double are bad for calculating monetary values. Why?");
+        System.out.println(1.03-.42);
+        System.out.println(1.00-9*.10);
+        System.out.println("Use BigDecimal, int or long instead!");
+        BigDecimal TEN_CENTS = new BigDecimal(".01");
+        BigDecimal funds = new BigDecimal("1.00");
+        BigDecimal discount = new BigDecimal(9);
+        System.out.println(funds.subtract(discount).multiply(TEN_CENTS));
+        System.out.println("Can we be more efficient?");
+        int itemsBought = 0;
+        int funds1 = 100;
+        for (int price = 10; funds1 >= price; price += 10) {
+            itemsBought++;
+            funds1 -= price;
+        }
+        System.out.println(itemsBought + " items bought.");
+        System.out.println("Money left over: "+ funds1 + " cents");
+    }
 }
