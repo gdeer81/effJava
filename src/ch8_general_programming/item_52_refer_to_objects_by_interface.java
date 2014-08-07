e64b2ab472ba0865eadeb311d1daab4e08fe613b
pance <pbarnett888@gmail.com>
2014-08-07 11:56:23 -0500

    Refactor method names and improve comments

--- src/ch8_general_programming/item_52_refer_to_objects_by_interface.java
+++ src/ch8_general_programming/item_52_refer_to_objects_by_interface.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 public class item_52_refer_to_objects_by_interface {
 
-    // We have a method that consumes a Collection
-    static void consumeCollection(Collection<String> list) {
+    // We have a method that consumes a Collection and prints it
+    static void consumeCollectionAndPrint(Collection<String> list) {
         for(String s : list) {
             System.out.println("...processing : " + s);
         }
     }
-    // We have an inferior method that consumes an ArrayList
+    // We have an inferior method that consumes an ArrayList and prints it
     // Note that this method takes an ArrayList, but doesn't need any implementation specific to an ArrayList
     // This method could easily accept any type of Collection to do the same operation
-    static void consumeCollectionBadly(ArrayList<String> list) {
+    static void consumeCollectionBadlyAndPrint(ArrayList<String> list) {
         for(String s : list) {
             System.out.println("...processing : " + s);
         }
@@ -42,16 +42,16 @@ public class item_52_refer_to_objects_by_interface {
         hashSet.add("Qux");
         hashSet.add("Foo");
 
-        // If we try using our inferior method on each collection, we would have to cast each collection
+        // If we try using our inferior method on each collection, we would have to cast each collection to an ArrayList
         System.out.println("***Processing ArrayList with consumeCollectionBadly");
-        consumeCollectionBadly(arrayList);
+        consumeCollectionBadlyAndPrint(arrayList);
 
         // Using our superior method, we can be flexible in how we build collections to give to the method
         System.out.println("***Processing ArrayList with consumeCollection");
-        consumeCollection(arrayList);
+        consumeCollectionAndPrint(arrayList);
         System.out.println("***Processing Stack with consumeCollectionBadly");
-        consumeCollection(stack);
+        consumeCollectionAndPrint(stack);
         System.out.println("***Processing HashSet with consumeCollectionBadly");
-        consumeCollection(hashSet);
+        consumeCollectionAndPrint(hashSet);
     }
 }
