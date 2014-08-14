80303dcc35d6eb631c58ee076ae4f7e7d3be7966
Dale Kocian <kocian.dale@heb.com>
2014-08-14 12:10:51 -0500

    Item 2: Consider a builder when faced with many constructor parameters - DK

---
+++ src/ch2_creating_and_destroying_objects/item2/java_beans_pattern/Main.java
@@ -0,0 +1,15 @@
+package ch2_creating_and_destroying_objects.item2.java_beans_pattern;
+
+/**
+ * Created by k557782 on 8/14/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts();
+        cocaCola.setServingSize(240);
+        cocaCola.setServings(8);
+        cocaCola.setCalories(100);
+        cocaCola.setSodium(35);
+        cocaCola.setCarbohydrate(27);
+    }
+}
---
+++ src/ch2_creating_and_destroying_objects/item2/java_beans_pattern/NutritionFacts.java
@@ -0,0 +1,42 @@
+package ch2_creating_and_destroying_objects.item2.java_beans_pattern;
+
+/**
+ * Created by k557782 on 8/14/2014.
+ */
+public class NutritionFacts {
+    // Parameters initialized to default values (if any)
+    private int servingSize = -1; // Required; no default value
+    private int servings = -1;  //     "     "      "      "
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public NutritionFacts() {
+    }
+
+    // Setters
+    public void setServingSize(int val) {
+        servingSize = val;
+    }
+
+    public void setServings(int val) {
+        servings = val;
+    }
+
+    public void setCalories(int val) {
+        calories = val;
+    }
+
+    public void setFat(int val) {
+        fat = val;
+    }
+
+    public void setSodium(int val) {
+        sodium = val;
+    }
+
+    public void setCarbohydrate(int val) {
+        carbohydrate = val;
+    }
+}
